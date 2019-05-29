d0711eb6a42213179bf8ddf3a9e49c08eb46e180
Vasya Pupkin <vasya@localhost>
2019-05-29 09:07:21 +0300

    2 com

--- functions/main.js
+++ functions/main.js
@@ -5,11 +5,11 @@ function getSolutions(a, b, c) {
   let x1, x2;
   if (D > 0) {
     x1 = ((-b - Math.sqrt(D))/(2 * a));
-    x2 = ((-b + Math.sqrt(D))/(2 * a));
+   x2 = ((-b + Math.sqrt(D))/(2 * a));
     return { roots: [x1, x2], D: D };
-  } else if (D < 0) {
-    return { D: D };
-  } else if (D == 0) {
+ } else if (D < 0) {
+   return { D: D };
+ } else if (D == 0) {
     x1 = -b/2 * a;
     return { roots: x1, D: D };
   };
@@ -17,14 +17,14 @@ function getSolutions(a, b, c) {
 
 function showSolutionsMessage(a, b, c) {
   let result = getSolutions(a, b, c);
-  console.log("Вычисляем корни квадратного уравнения: " + a + "x²" + "+" + b + "x" + "+" + c);
-  console.log("Значение дискриминанта: " + result.D);
-  if (result.D > 0) {
+ console.log("Вычисляем корни квадратного уравнения: " + a + "x²" + "+" + b + "x" + "+" + c);
+ console.log("Значение дискриминанта: " + result.D);
+ if (result.D > 0) {
     console.log("Уравнение имеет два корня. X₁ = " + result.roots[0] + ", " + "X₂ = " + result.roots[1]);
-  } else if (result.D == 0) {
+ } else if (result.D == 0) {
     console.log("Уравнение имеет один корень X₁ = " + result.roots);
   } else {
-    console.log("Уравнение не имеет вещественных корней");
+  console.log("Уравнение не имеет вещественных корней");
   };
 };
 
@@ -32,28 +32,34 @@ showSolutionsMessage( 1, 2, 3 );
 showSolutionsMessage( 7, 20, -3 );
 showSolutionsMessage( 2, 4, 2);
 
+
 //Задача 2
+function getAverageScore(data) {
+  
+  
+  for ( let prop in data ) {
+    let value = data[ prop ];
+    console.log( `Свойство ${prop}, значение: ${averageData}` );
+    
+    }
+  };
+  
+  
+  
+  function getAverageMark(data) {
+
+  const sumArr = data.algebra.reduce((partial_sumArr, a) => partial_sumArr + a,0); 
+      let averageData = sumArr / data.algebra.length;
+    console.log(averageData);
+    
+    return averageData;
+    };
+    
+
+console.log( getAverageScore({
+  algebra: [ 4, 2, 3, 2 ],
+  russian: [ 2, 4, 5, 2 ]
+}));
 
 
-function getAverageScore( data ) {
-console.log(data);
- function averageArr() {
-  
-  let sum = res.reduce((partial_sum, a) => partial_sum + a,0); 
- return sum;
- }
- 
-console.log(res);
-  
-};
-getAverageScore({
-  algebra: [ 2, 4, 5, 2, 3, 4 ],
-  geometry: [ 2, 4, 5 ],
-  russian: [ 3, 3, 4, 5 ],
-  physics: [ 5, 5 ],
-  music: [ 2, 2, 6 ],
-  english: [ 4, 4, 3 ],
-  poetry: [ 5, 3, 4 ],
-  chemistry: [ 2 ],
-  french: [ 4, 4 ]
-});
+
