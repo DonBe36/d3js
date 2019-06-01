97cb39238319943026492b5cb03cbe9e942f8c06
Vasya Pupkin <vasya@localhost>
2019-06-01 14:04:11 +0300

    4com

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,4 @@
+"use strict"
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -9,20 +10,53 @@ function calculateMortgage() {
     span.textContent = result;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
+function calculateTotalMortgage(percent, contribution, amount, date) {   
+     if (percent > 0) {  
+       percent = + percent;
+     } else if (percent != 0 || percent == 0) {
+         return (`"Процентная ставка" имеет неверное значение ${percent}”.`);
+     }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
-}
+     if (contribution >= 0) {  
+       contribution = +contribution;
+     } else if (contribution !=  0) {
+         return (`"Начальный взнос" имеет неверное значение ${contribution}”.`); 
+     }
+  
+     if (amount >= 0) {  
+       amount = +amount;
+     } else if (amount !=  0) {
+         return (`"Общая стоимость" имеет неверное значение ${amount}”.`); 
+     };
+
+   let curDate  = new Date(); 
+   date = new Date(window.date.value);
+   let periodPay = 0 - ((curDate.getFullYear() -  date.getFullYear()) * 12) - (curDate.getMonth() - date.getMonth());
+   date = periodPay; 
+   percent = percent / 1200; 
+   let monthPay = amount * (percent + percent / (((1 + percent)**date) - 1)); 
+   let  totalAmount = monthPay * date;  
+   console.log(totalAmount.toFixed(2));
+   return totalAmount.toFixed(2); 
+};
 
 function sayHello() {
     let name = window.personName.value;
     let greeting = getGreeting(name);
     let span = window.helloResult;
     span.textContent = greeting;
-}
+};
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
-}
+  let greeting = "";
+  
+  if (name === "" || name === String(undefined) || name === String(null)) {
+    greeting = "Привет, мир! Меня зовут Аноним.";
+    console.log(greeting);
+    return greeting;
+  } else {
+      greeting = `Привет, мир! Меня зовут ${name}.`;
+      console.log(greeting);
+      return greeting 
+  };  
+};
